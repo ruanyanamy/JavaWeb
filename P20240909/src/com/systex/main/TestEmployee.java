3716c6da9a9d965132e8820684e11d0d0b0a7c78
Juan00 <amy061025@gmail.com>
2024-09-10 22:23:27 +0800

    JavaCourse

---
+++ P20240909/src/com/systex/main/TestEmployee.java
@@ -0,0 +1,16 @@
+package com.systex.main;
+import com.systex.model.*;
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp1 = new Employee();
+		emp1.name = "張友福";
+		emp1.title = "經理";
+		emp1.department = "IT";
+		emp1.salary = 85000;
+
+		System.out.println(emp1);    //快捷鍵sout -> System.out.println
+	}
+
+}
