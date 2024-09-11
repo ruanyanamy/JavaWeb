0c19532958ca2bd4690b0346572d4db78369717b
Juan00 <amy061025@gmail.com>
2024-09-11 17:02:21 +0800

    JavaCourse

--- P20240911/src/com/systex/main/TestEquals.java
+++ P20240911/src/com/systex/main/TestEquals.java
@@ -8,8 +8,16 @@ public class TestEquals {
 		MyDate d1 = new MyDate(11,9,2024);
 		MyDate d2 = new MyDate(11,9,2024);
 		
+		System.out.println("d1= "+d1);
+		System.out.println("d2= "+d2);
+		
 		System.out.println(d1 == d2);
 		System.out.println(d1.equals(d2));  //MyDate有Override equals的method
+		
+		Integer wAmount = 10000; //Auto-Boxing
+		int balance = 5000; 
+		balance = balance + wAmount; //Auto-Unboxing
+		System.out.println("balance= "+balance);
 	}
 
 }
